8e1e2359f5459f94edd45c9f65c22023dd891ac0
Tyler Nichols <tylernchls@gmail.com>
2016-12-30 10:28:34 -1000

    small code refactor

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const styles = require('./html_files/styles.js');
 /*
 This server works with client.js only when connected on port 8080. Client
 sends request header and will check for that data, grab, and sent it back.
-
-
 */
+
+
 
 let server = net.createServer((socket) => {
   socket.on('error', (err) => {
@@ -25,6 +25,7 @@ let server = net.createServer((socket) => {
 
   socket.on(EVENT_DATA, (data) => {
     let reqHeaderArr = data.toString().split(' ');
+
     let date = new Date();
     console.log(reqHeaderArr);
 
@@ -33,51 +34,51 @@ let server = net.createServer((socket) => {
 
           process.stdout.write(data);
           socket.write(`HTTP/1.1 200 OK
-                        Server: ${SERVER}
-                        Date: ${date}
-                        Content-Type: text/html; charset=utf-8
-                        Content-Length: ${index.length}
-                        Connection: keep-alive\n\n`);
+Server: ${SERVER}
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${index.length}
+Connection: keep-alive\n\n`);
           socket.write(index);
           return socket.end();
 
-      } else if(reqHeaderArr[0] === 'GET' && reqHeaderArr[1] === '/' || reqHeaderArr[1] === '/helium.html') {
+      } else if(reqHeaderArr[0] === 'GET' && reqHeaderArr[1] === '/helium.html') {
         process.stdout.write(data);
           socket.write(`HTTP/1.1 200 OK
-                        Server: ${SERVER}
-                        Date: ${date}
-                        Content-Type: text/html; charset=utf-8
-                        Content-Length: ${helium.length}
-                        Connection: keep-alive\n\n`);
+Server: ${SERVER}
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${helium.length}
+Connection: keep-alive\n\n`);
           socket.write(helium);
           return socket.end();
-      } else if(reqHeaderArr[0] === 'GET' && reqHeaderArr[1] === '/' || reqHeaderArr[1] === '/hydrogen.html') {
+      } else if(reqHeaderArr[0] === 'GET' && reqHeaderArr[1] === '/hydrogen.html') {
         process.stdout.write(data);
           socket.write(`HTTP/1.1 200 OK
-                        Server: ${SERVER}
-                        Date: ${date}
-                        Content-Type: text/html; charset=utf-8
-                        Content-Length: ${hydrogen.length}
-                        Connection: keep-alive\n\n`);
+Server: ${SERVER}
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${hydrogen.length}
+Connection: keep-alive\n\n`);
           socket.write(hydrogen);
           return socket.end();
       } else if(reqHeaderArr[0] === 'GET' && reqHeaderArr[1] === '/css/styles.css') {
         process.stdout.write(data);
           socket.write(`HTTP/1.1 200 OK
-                        Server: ${SERVER}
-                        Date: ${date}
-                        Content-Type: text/css; charset=utf-8
-                        Content-Length: ${styles.length}
-                        Connection: keep-alive\n\n`);
+Server: ${SERVER}
+Date: ${date}
+Content-Type: text/css; charset=utf-8
+Content-Length: ${styles.length}
+Connection: keep-alive\n\n`);
           socket.write(styles);
           return socket.end();
       } else {
          socket.write(`HTTP/1.1 404
-                        Server: ${SERVER}
-                        Date: ${date}
-                        Content-Type: text/html; charset=utf-8
-                        Content-Length: ${notFound_error.length}
-                        Connection: keep-alive\n\n`);
+Server: ${SERVER}
+Date: ${date}
+Content-Type: text/html; charset=utf-8
+Content-Length: ${notFound_error.length}
+Connection: keep-alive\n\n`);
         socket.write(notFound_error);
         return socket.end();
       }
