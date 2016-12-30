9fa21b2b012b184162a6bcacae2d0e69e40e9481
Tyler Nichols <tylernchls@gmail.com>
2016-12-29 18:41:01 -1000

    added ability to switch ports based on what user enters in cmd line;

--- client.js
+++ client.js
@@ -1,18 +1,27 @@
 const net = require('net');
-const PORT = 8080; // only to connect to server.js
+let port;
 const EVENT_DATA = 'data';
 
-const HOST= process.argv[2];
+const hostId = process.argv[2].split('/')[0];
+
+
+const host_path = process.argv[2].split('/')[1];
+
 
 /*
-Allows client to make data requests from any website using port 80.
-Simulates curl
+port 8080 used to connect to server.js
+port 80 used to connect to outside sites
 */
+if(hostId === 'localhost') {
+  port = 8080;
+} else {
+  port = 80;
+}
 
-const client = net.connect(80, HOST, () => {
+const client = net.connect(port, hostId, () => {
   let date = new Date();
   process.stdin.on(EVENT_DATA, () => {
-    client.write(`GET / HTTP/1.1
+  client.write(`GET /host_path HTTP/1.1
 Accept: text/html, charset=utf-8
 Host: localhost
 Date: ${date}
